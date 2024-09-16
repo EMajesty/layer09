ef2e5cab67f574366d2fee9cc0c6e66cb1ad7203
EMajesty <lassi.laitinen@protonmail.com>
2024-09-16 11:26:20 +0300

    hell satan

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,22 @@
 use rp_pico::entry;
 use panic_halt as _;
 use embedded_hal::digital::OutputPin;
-use embedded_hal::spi::MODE_0;
+use embedded_hal::spi::MODE_3;
 use rp_pico::hal::prelude::*;
 use rp_pico::hal::pac;
 use rp_pico::hal::spi::Spi;
 use rp_pico::hal::gpio::FunctionSpi;
 use rp_pico::hal;
 use rp_pico::hal::fugit::RateExtU32;
-use st7789::{Orientation, ST7789};
 use display_interface_spi::SPIInterface;
 use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::prelude::*;
 use embedded_graphics::primitives::*;
+use embedded_graphics::image::*;
+use mipidsi::{Builder, models::ST7789};
+
+const SCR_WT: u16 = 240;
+const SCR_HT: u16 = 320;
 
 #[entry]
 fn stupid() -> ! {
@@ -63,48 +67,43 @@ fn stupid() -> ! {
     let spi_pin_layout = (mosi, sclk);
 
     let spi = Spi::<_, _, _, 8>::new(spi_device, spi_pin_layout)
-        .init(&mut pac.RESETS, 125_000_000u32.Hz(), 16_000_000u32.Hz(), MODE_0);
+        .init(&mut pac.RESETS, 125_000_000u32.Hz(), 32_000_000u32.Hz(), MODE_3);
 
-    let display_interface = SPIInterface::new(spi, dc, cs);
-    let mut display = ST7789::new(display_interface, Some(rst), Some(backlight), 240, 320);
+    let di = SPIInterface::new(spi, dc);
+    let mut display = Builder::new(ST7789, di)
+        .reset_pin(rst)
+        .init(&mut delay)?;
+    // let mut display = ST7789::new(display_interface, Some(rst), Some(backlight), SCR_WT, SCR_HT);
 
-    display.init(&mut delay).unwrap();
-    display.set_orientation(Orientation::PortraitSwapped).unwrap();
+    // display.init(&mut delay).unwrap();
+    // display.set_orientation(Orientation::PortraitSwapped).unwrap();
 
-    let line1 = Line::new(Point::new(100, 20), Point::new(100, 220))
-        .into_styled(PrimitiveStyle::with_stroke(RgbColor::WHITE, 10));
-    let line2 = Line::new(Point::new(100, 20), Point::new(160, 20))
-        .into_styled(PrimitiveStyle::with_stroke(RgbColor::WHITE, 10));
-    let line3 = Line::new(Point::new(100, 105), Point::new(160, 105))
-        .into_styled(PrimitiveStyle::with_stroke(RgbColor::WHITE, 10));
+    // let raw_image_data = ImageRawLE::new(include_bytes!("../assets/image.raw"), 240);
+    // let image = Image::new(&raw_image_data, Point::new(0, 0));
+    // image.draw(&mut display).unwrap();
 
-    let triangle = Triangle::new(
-        Point::new(240, 100),
-        Point::new(240, 140),
-        Point::new(320, 120),
-    )
-    .into_styled(PrimitiveStyle::with_fill(Rgb565::GREEN));
-
-    display.clear(Rgb565::BLACK).unwrap();
-    line1.draw(&mut display).unwrap();
-    line2.draw(&mut display).unwrap();
-    line3.draw(&mut display).unwrap();
-    triangle.draw(&mut display).unwrap();
-
-    let mut scroll = 1u16; // absolute scroll offset
-    let mut direction = true; // direction
-    let scroll_delay = 20u8; // delay between steps
     loop {
-        delay.delay_ms(scroll_delay);
-        display.set_scroll_offset(scroll).unwrap();
-
-        if scroll % 80 == 0 {
-            direction = !direction;
-        }
-
-        match direction {
-            true => scroll += 1,
-            false => scroll -= 1,
-        }
+        continue;
+        // let mut i = 0;
+        //
+        // while i < SCR_HT {
+        //     let mut j = 0;
+        //     while j < SCR_WT {
+        //         display.set_pixel(j, i, 59423).unwrap();
+        //         j = j + 1;
+        //     }
+        //     i = i + 1;
+        // }
+        //
+        // let mut i = 0;
+        //
+        // while i < SCR_HT {
+        //     let mut j = 0;
+        //     while j < SCR_WT {
+        //         display.set_pixel(j, i, 2015).unwrap();
+        //         j = j + 1;
+        //     }
+        //     i = i + 1;
+        // }
     }
 }
